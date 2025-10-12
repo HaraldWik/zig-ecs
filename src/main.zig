274dd2385833157e832165870521729af8d030d2
HaraldWik <wikhallharald@gmail.com>
2025-10-12 16:45:23 +0200

    Revamp

--- src/main.zig
+++ src/main.zig
@@ -5,7 +5,7 @@ pub const ecs_lib = struct {
         _,
 
         pub fn get(self: @This(), comptime T: type, ecs: anytype) ?T {
-            return @field(ecs.layout, @typeName(T)).items[@intFromEnum(self)];
+            return ecs.getLayoutComp(T).items[@intFromEnum(self)];
         }
 
         pub fn getPtr(self: @This(), comptime T: type, ecs: anytype) ?*T {
@@ -14,7 +14,7 @@ pub const ecs_lib = struct {
         }
 
         pub fn set(self: @This(), comptime T: type, val: T, ecs: anytype) void {
-            @field(ecs.layout, @typeName(T)).items[@intFromEnum(self)] = val;
+            ecs.getLayoutComp(T).items[@intFromEnum(self)] = val;
         }
 
         pub fn getGeneration(self: @This(), ecs: anytype) usize {
@@ -23,31 +23,32 @@ pub const ecs_lib = struct {
     };
 
     pub fn Ecs(comps: []const type) type {
-        const L: type = @Type(.{ .@"struct" = .{
-            .fields = fields: {
-                var fields: [comps.len]std.builtin.Type.StructField = undefined;
-                for (comps, &fields) |comp, *field| field.* = .{
-                    .name = @typeName(comp),
-                    .type = std.ArrayList(?comp),
-                    .alignment = @alignOf(std.ArrayList(?comp)),
-                    .is_comptime = false,
-                    .default_value_ptr = null,
-                };
-                break :fields &fields;
-            },
-            .decls = &.{},
-            .is_tuple = false,
-            .layout = .auto,
-        } });
+        const types: [comps.len]type = types: {
+            var types: [comps.len]type = @splat(@TypeOf(null));
+            for (comps, &types) |comp, *@"type"| @"type".* = std.ArrayList(comp);
+            break :types types;
+        };
+
+        const kvs = kvs: {
+            var kvs: [comps.len]std.StaticStringMap(usize).KV = undefined;
+            for (comps, &kvs, 0..) |comp, *kv, i| kv.* = .{ .key = @typeName(comp), .value = i };
+            break :kvs kvs;
+        };
 
         return struct {
-            pub const Layout = L;
-
             allocator: std.mem.Allocator,
 
             layout: Layout = undefined,
+            // info: []std.meta.Int(.unsigned, comps.len),
             generation: std.ArrayList(usize) = .empty,
             next: std.Deque(Entity) = .empty,
+
+            pub const Layout: type = std.meta.Tuple(&types);
+
+            pub fn getCompIndex(comptime T: type) usize {
+                inline for (kvs) |kv| if (std.mem.eql(u8, kv.key, @typeName(T))) return kv.value;
+                @panic("invalid type of " ++ @typeName(T));
+            }
 
             pub fn init(allocator: std.mem.Allocator, capacity: ?usize) !@This() {
                 var self: @This() = .{
@@ -55,19 +56,23 @@ pub const ecs_lib = struct {
                     .generation = try .initCapacity(allocator, capacity orelse 1),
                     .next = try .initCapacity(allocator, 1),
                 };
-                inline for (comps) |comp| @field(self.layout, @typeName(comp)) = try .initCapacity(allocator, capacity orelse 1);
+                inline for (comps) |comp| self.layout[comptime getCompIndex(comp)] = try .initCapacity(allocator, capacity orelse 1);
                 return self;
             }
 
             pub fn deinit(self: *@This()) void {
                 self.generation.deinit(self.allocator);
-                inline for (comps) |comp| @field(self.layout, @typeName(comp)).deinit(self.allocator);
+                inline for (comps) |comp| self.layout[comptime getCompIndex(comp)].deinit(self.allocator);
+            }
+
+            pub fn getLayoutComp(self: @This(), comptime T: type) std.ArrayList(T) {
+                return self.layout[comptime getCompIndex(T)];
             }
 
             pub fn add(self: *@This()) !Entity {
                 const front: usize = @intFromEnum(self.next.popFront() orelse @as(Entity, @enumFromInt(self.generation.items.len)));
                 try self.generation.insert(self.allocator, front, 0);
-                inline for (comps) |comp| try @field(self.layout, @typeName(comp)).insert(self.allocator, front, null);
+                inline for (comps) |comp| try self.layout[comptime getCompIndex(comp)].insert(self.allocator, front, undefined);
 
                 return @enumFromInt(front);
             }
@@ -79,14 +84,14 @@ pub const ecs_lib = struct {
 
             pub fn query(self: @This(), comptime T: []const type, allocator: std.mem.Allocator) !std.ArrayList(Entity) {
                 var len: usize = std.math.maxInt(usize);
-                inline for (comps) |comp| len = @min(len, @field(self.layout, @typeName(comp)).items.len);
+                inline for (comps) |comp| len = @min(len, self.getLayoutComp(comp).items.len);
 
                 var out: std.ArrayList(Entity) = try .initCapacity(allocator, 128);
 
                 for (0..len) |i| {
                     var found: usize = 0;
                     inline for (T) |comp| {
-                        if (@field(self.layout, @typeName(comp)).items[i] != null) found += 1;
+                        if (self.getLayoutComp(comp).items[i] != null) found += 1;
                     }
 
                     if (found == T.len) try out.append(allocator, @enumFromInt(i));
@@ -99,14 +104,14 @@ pub const ecs_lib = struct {
                 @memset(buffer, @enumFromInt(0));
 
                 var len: usize = std.math.maxInt(usize);
-                inline for (comps) |comp| len = @min(len, @field(self.layout, @typeName(comp)).items.len);
+                inline for (comps) |comp| len = @min(len, self.getLayoutComp(comp).items.len);
 
                 var out: usize = 0;
 
                 for (0..len) |i| {
                     var found: usize = 0;
                     inline for (T) |comp| {
-                        if (@field(self.layout, @typeName(comp)).items[i] != null) found += 1;
+                        if (self.getLayoutComp(comp).items[i] != 0) found += 1;
                     }
 
                     if (found == T.len) {
@@ -147,7 +152,7 @@ pub fn main() !void {
     const thing: ecs_lib.Entity = try ecs.add();
     thing.set(u32, 420, ecs);
 
-    std.debug.print("u32: {any}\nf32: {any}\n", .{ @field(ecs.layout, @typeName(u32)).items, @field(ecs.layout, @typeName(f32)).items });
+    std.debug.print("u32: {any}\nf32: {any}\n", .{ ecs.layout[comptime @TypeOf(ecs).getCompIndex(u32)].items, ecs.layout[comptime @TypeOf(ecs).getCompIndex(f32)].items });
 
     var query: [1028]ecs_lib.Entity = undefined;
     const n = try ecs.queryBuffer(&.{u32}, &query);
